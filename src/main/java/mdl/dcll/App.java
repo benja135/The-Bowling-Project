d3e47671664eadcf3c524d6379b1b828146c196f
Matteo <matteo.mougeot@gmail.com>
2016-03-09 17:42:33 +0100

    Merge branch 'master' of github.com:benja135/The-Bowling-Project
    # Conflicts:
    #	src/main/java/mdl/dcll/App.java

--- src/main/java/mdl/dcll/App.java
+++ src/main/java/mdl/dcll/App.java
@@ -7,6 +7,14 @@ package mdl.dcll;
 public class App
 {
 
+    public static void initialiseStrike(int[][] tableau,int nbstrike){
+
+        tableau[nbstrike][0] = 0; // doit atteindre 2 pour arreter le strike
+        tableau[nbstrike][1] = 10; // score total du strike 10 de départ car bonus de 10
+
+
+    }
+
     // retourne le score de la partie passé en paramètre
     public static int calculerResultat(String res){
         int score = 0;
@@ -15,6 +23,7 @@ public class App
         boolean spare = false;
         boolean strike = false;
         boolean rienTouche = false;
+        int coupStrike = 2;
 
         for (int i=0; i< res.length(); i++) {
 
@@ -27,19 +36,28 @@ public class App
                 rienTouche = true;
             }
 
+            if(res.charAt(i) == 'X'){
+                bonus = 10;
+                strike = true;
+            }
+
 
 
 // a implenté : aucune boule touché au premier lancé
             if(i%2 == 0){ //tous les premiers pair
-                if(spare){
+
+                if(spare){ // si spare
                     score += bonus;
                     score += res.charAt(i)-'0';
                     spare = false;
                     bonus = 0;
                 }
-                else if(rienTouche) {
+                else if(rienTouche) { //si pas de boule touché
                     scoreAvalider = 0;
                     rienTouche = false;
+                }
+                else if(strike){ // si strike
+                    scoreAvalider += bonus;
                 }
                 else{
 
@@ -73,22 +91,68 @@ public class App
             }
 
         }
+
+        //************************************************//
+        //strike:
+
+        int[][] tab = new int [10][10];
+
+        int nbstrike=0;
+
+        if(strike){
+            nbstrike++;
+            initialiseStrike(tab,nbstrike);
+
+            for(int i=0; i< nbstrike;i++) {
+                tab[nbstrike][0]++;
+
+                if (tab[nbstrike][0] < 3) {
+
+                    if (res.charAt(i) == '/' || res.charAt(i) == 'X') {
+                        tab[nbstrike][1] += 10;
+                    }
+                    else if(res.charAt(i) == '/'){
+                        tab[nbstrike][1] += 0;
+                    }
+                    else{
+                        tab[nbstrike][1] += res.charAt(i)-'0';
+                    }
+                }
+
+
+            }
+
+        }
+
+        ///***********************************************//
+
+
+
         return score;
 
+
+
+
+
+
     }
+
+
 
     public static void main( String[] args )
     {
         String resultat1 ="XXXXXXXXXXXX";
         String resultat2="9_9_9_9_9_9_9_9_9_9_";
         String resultat3="5/5/5/5/5/5/5/5/5/5/5";
-        String resultat4="1234567891";
 
 
 
-        System.out.println("normalement 46 : " + calculerResultat(resultat4));
+
+
         System.out.println("normalement 150 : " + calculerResultat(resultat3));
         System.out.println("normalement 90 : " + calculerResultat(resultat2));
+        System.out.println("normalement 300 : " + calculerResultat(resultat1));
+
 
 
 
